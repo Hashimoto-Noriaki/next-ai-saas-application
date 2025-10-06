2505e91570ebd595a30e833bc0829dbf1ad5b4a6
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-07 02:20:29 +0900

    dashbordへのリンクを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from '@/components/ui/button'
+import Link from 'next/link' 
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Button>ダッシュボードへ移動</Button>
+        <Button>
+          <Link href={'/dashbord'}>ダッシュボードへ移動</Link>
+        </Button>
       </main>
     </div>
   );
