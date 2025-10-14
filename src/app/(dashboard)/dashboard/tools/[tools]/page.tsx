e9ce0684d76583e63a49126addad0d89ea579fac
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-15 00:04:29 +0900

    AI画像生成コンポーネント作成

--- src/app/(dashboard)/dashboard/tools/[tools]/page.tsx
+++ src/app/(dashboard)/dashboard/tools/[tools]/page.tsx
@@ -17,7 +17,7 @@ const ToolPage = async ({ params }: {params:Promise<{ tool:string }>}) => {
     return (
         <PageContainer>
             <PageHeader title = {tool.title} description = {tool.description}/>
-                <div>
+                <div className="max-w-2xl">
                     <ToolComponent/>
                 </div>
         </PageContainer>
--- src/components/dashboard/tools/image-generator.tsx
+++ src/components/dashboard/tools/image-generator.tsx
@@ -1,6 +1,28 @@
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
+
 export const ImageGenerator = () => {
     return (
-        <div>image-generator</div>
+        <div className="space-y-6">
+            <div className="space-y-4">
+                <form action="" className="space-y-4">
+                    <div className="space-y-2">
+                        <Label htmlFor="keyword">キーワード</Label>
+                        <Input 
+                            id="keyword"
+                            name="keyword"
+                            placeholder="作成したい画像のキーワードを入力(例:海、山、都会、自然)"
+                            required
+                        />
+                    </div>
+                    {/* submit button */}
+                    <Button>画像を生成する</Button>
+                </form>
+            </div>
+            { /* image preview */ }
+            <div></div>
+        </div>
     )
 }
 
