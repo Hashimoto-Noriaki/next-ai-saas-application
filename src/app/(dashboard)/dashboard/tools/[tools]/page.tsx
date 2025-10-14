018242a758244574b6326d1719d33d8cd60af1c0
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-14 22:44:50 +0900

    toolがない時は、not foundページを表示

--- src/app/(dashboard)/dashboard/tools/[tools]/page.tsx
+++ src/app/(dashboard)/dashboard/tools/[tools]/page.tsx
@@ -3,6 +3,12 @@ import { tools } from '@/config/tools'
 const ToolPage = async ({ params }: {params:Promise<{ tool:string }>}) => {
     const toolType = (await params).tools as ToolType
     const tool = tools[toolType];
+
+    //toolがない時はnot foundページを表示
+    if(!tool){
+        notFound()
+    }
+
     return (
         <div>
             <h1>{tool.title}</h1>
