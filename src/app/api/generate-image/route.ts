fb10082b40942ca845f6726b9c609b283407d626
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-15 13:55:54 +0900

    formDataで入力した値の取得とServerActionsの実行

--- src/app/api/generate-image/route.ts
+++ src/app/api/generate-image/route.ts
@@ -1,15 +1,24 @@
+import { NextResponse } from 'next/server'
 import axios from "axios";
 import FormData from "form-data";
 
-export default function POST(req: Request){
+export async function POST(req: Request){
+    const { keyword } = await req.json();
+    console.log("keyword",keyword)
+
     try {
         const payload = {
             prompt: "Lighthouse on a cliff overlooking the ocean",
             output_format: "png"
         };
+
+        const formData = new FormData();
+        formData.append("prompt",payload.prompt)
+        formData.append("output_format",payload.output_format)
+
         const response = await axios.postForm(
             `https://api.stability.ai/v2beta/stable-image/generate/core`,
-            axios.toFormData(payload, new FormData()),
+            formData,
             {
                 validateStatus: undefined,
                 responseType: "arraybuffer",
@@ -24,6 +33,7 @@ export default function POST(req: Request){
             throw new Error(`API error: ${response.status}`)
         }
         console.log(response.data)
+        return NextResponse.json(response.data)
     } catch(error) {
         console.error("Error generate image:",error);
         return NextResponse.json(
--- src/components/dashboard/tools/image-generator.tsx
+++ src/components/dashboard/tools/image-generator.tsx
@@ -21,21 +21,19 @@ export const ImageGenerator = () => {
 
     return (
         <div className="space-y-6">
-            <div className="space-y-4">
-                <form action={generateImage} className="space-y-4">
-                    <div className="space-y-2">
-                        <Label htmlFor="keyword">キーワード</Label>
-                        <Input 
-                            id="keyword"
-                            name="keyword"
-                            placeholder="作成したい画像のキーワードを入力(例:海、山、都会、自然)"
-                            required
-                        />
-                    </div>
-                    {/* submit button */}
-                    <Button>画像を生成する</Button>
-                </form>
-            </div>
+            <form action={generateImage} className="space-y-4">
+                <div className="space-y-2">
+                    <Label htmlFor="keyword">キーワード</Label>
+                    <Input 
+                        id="keyword"
+                        name="keyword"
+                        placeholder="作成したい画像のキーワードを入力(例:海、山、都会、自然)"
+                        required
+                    />
+                </div>
+                {/* submit button */}
+                <Button>画像を生成する</Button>
+            </form>
             { /* image preview */ }
             <div></div>
         </div>
